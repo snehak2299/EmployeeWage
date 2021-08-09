3d0c44db9e78d978f0111d2652682aca4dac6832
snehak2299 <skanase2299@gmail.com>
2021-08-09 15:49:48 +0530

    [sneha]:for full day employee wage is calculated

--- empwage.java
+++ empwage.java
@@ -1,12 +1,17 @@
 public class empwage {
     public static void main (String[]args){
     int IS_EMP_PRESENT= 1;
+    int WAGE_PER_HR =20;
+    int empHrs = 0;
     double empcheck = Math.floor((Math.random()* 10) %2 );
     if(empcheck==IS_EMP_PRESENT){
      System.out.println("employee is present");
+     empHrs=8;
 	}
     else{
      System.out.println("employee is absent");
 	}
+    int EmployeeWage = empHrs * WAGE_PER_HR;
+    System.out.println("employee wage is:"+ EmployeeWage);
   }
 }
