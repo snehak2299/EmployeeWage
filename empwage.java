9229e1d18ff8a52a6fa2e0cfad908d42958c2367
snehak2299 <skanase2299@gmail.com>
2021-08-09 14:56:28 +0530

    [sneha]:to check if employee is present or absent

--- empwage.java
+++ empwage.java
@@ -1,4 +1,12 @@
 public class empwage {
     public static void main (String[]args){
+    int IS_EMP_PRESENT= 1;
+    double empcheck = Math.floor((Math.random()* 10) %2 );
+    if(empcheck==IS_EMP_PRESENT){
+     System.out.println("employee is present");
+	}
+    else{
+     System.out.println("employee is absent");
+	}
   }
 }
