a5c002e6f3ed61324886e7292d63d66d9e517f84
snehak2299 <skanase2299@gmail.com>
2021-08-09 21:35:01 +0530

    part time wage and full time wage is calculated

--- empwage.java
+++ empwage.java
@@ -1,15 +1,16 @@
 public class empwage {
     public static void main (String[]args){
-    int IS_EMP_PRESENT= 1;
+    int IS_FULL_TIME= 2;
+    int IS_HALF_TIME =1;
     int WAGE_PER_HR =20;
     int empHrs = 0;
-    double empcheck = Math.floor((Math.random()* 10) %2 );
-    if(empcheck==IS_EMP_PRESENT){
+    double empcheck = Math.floor((Math.random()* 10) %3 );
+    if(empcheck==IS_FULL_TIME){
      System.out.println("employee is present");
      empHrs=8;
 	}
-    else{
-     System.out.println("employee is absent");
+    else if(empcheck==IS_HALF_TIME){
+     empHrs=4;
 	}
     int EmployeeWage = empHrs * WAGE_PER_HR;
     System.out.println("employee wage is:"+ EmployeeWage);
