445cbc029790a8ab824768e824daaa7b7eafb0fa
snehak2299 <skanase2299@gmail.com>
2021-08-10 10:16:54 +0530

    [sneha]:used switch case insted of if else

--- empwage.java
+++ empwage.java
@@ -1,16 +1,17 @@
 public class empwage {
+    public static final int IS_FULL_TIME= 2;
+    public static final int IS_HALF_TIME =1;
+    public static final int WAGE_PER_HR =20;
     public static void main (String[]args){
-    int IS_FULL_TIME= 2;
-    int IS_HALF_TIME =1;
-    int WAGE_PER_HR =20;
     int empHrs = 0;
-    double empcheck = Math.floor((Math.random()* 10) %3 );
-    if(empcheck==IS_FULL_TIME){
-     System.out.println("employee is present");
+    int empcheck =(int) Math.floor((Math.random()* 10) %3 );
+    switch(empcheck){
+     case IS_FULL_TIME:
      empHrs=8;
-	}
-    else if(empcheck==IS_HALF_TIME){
+     break;
+     case IS_HALF_TIME:
      empHrs=4;
+     break;
 	}
     int EmployeeWage = empHrs * WAGE_PER_HR;
     System.out.println("employee wage is:"+ EmployeeWage);
