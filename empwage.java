ab1528d8bf345ca52796ef68d51dba19279a8f81
snehak2299 <skanase2299@gmail.com>
2021-08-13 15:52:04 +0530

    [sneha]:calculate total wage till days are 20 or total hrs are 100

--- empwage.java
+++ empwage.java
@@ -2,12 +2,16 @@ public class empwage {
     public static final int IS_FULL_TIME= 2;
     public static final int IS_HALF_TIME =1;
     public static final int WAGE_PER_HR =20;
-    public static final int DAY_IN_MONTH = 2;
+    public static final int DAY_IN_MONTH = 20;
+    public static final int MAX_HRS = 100;
     public static void main (String[]args){
     int empHrs = 0;
+    int totalHrs = 0;
+    int totalWorkingDays=0;
     int totalempwage=0;
-    for (int days=0; days < DAY_IN_MONTH;days++){
-    int empcheck =(int) Math.floor((Math.random()* 10) %3 );
+    while (totalHrs<MAX_HRS && totalWorkingDays<DAY_IN_MONTH){
+     totalWorkingDays+=1;
+     int empcheck =(int) Math.floor((Math.random()* 10) %3 );
      switch(empcheck){
      case IS_FULL_TIME:
       empHrs=8;
@@ -15,11 +19,16 @@ public class empwage {
      case IS_HALF_TIME:
       empHrs=4;
      break;
+     default:
+	  empHrs = 0;
+	  break;
 	 }
-     int EmployeeWage = empHrs * WAGE_PER_HR;
-     totalempwage+= EmployeeWage;
-     System.out.println("employee wage is:"+ EmployeeWage);
+     totalHrs+=empHrs;
+     //totalWorkingDays+=totalWorkingDays;
+     System.out.println("total day:"+totalWorkingDays+"total hrs:"+totalHrs);
+     
     }
+    totalempwage=totalHrs*WAGE_PER_HR;
     System.out.println(" total employee wage is:"+ totalempwage);
   }
 }
