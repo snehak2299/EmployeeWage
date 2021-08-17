68e70f4cc33e27ec773f16e0a200ba271a492d90
snehak2299 <skanase2299@gmail.com>
2021-08-17 12:09:22 +0530

    [sneha]:refactored code to add ability to save wage for each company

--- ComputeWage.java
+++ ComputeWage.java
@@ -1,33 +1,47 @@
 public class ComputeWage{
 	public static final int IS_FULL_TIME= 2;
     public static final int IS_HALF_TIME =1;
+    public static String companyName;
+    public static int WAGE_PER_HR;
+    public static int DAY_IN_MONTH;
+    public static int MAX_HRS;
+    static int totalempwage;
+    
 
     ComputeWage(String companyName, int WAGE_PER_HR, int DAY_IN_MONTH, int MAX_HRS) {
-    int empHrs = 0;
-    int totalHrs = 0;
-    int totalWorkingDays=0;
-    int totalempwage=0;
-    while (totalHrs<MAX_HRS && totalWorkingDays<DAY_IN_MONTH){
-     totalWorkingDays+=1;
-     int empcheck =(int) Math.floor((Math.random()* 10) %3 );
-     switch(empcheck){
-     case IS_FULL_TIME:
-      empHrs=8;
-     break;
-     case IS_HALF_TIME:
-      empHrs=4;
-     break;
-     default:
-	  empHrs = 0;
-	  break;
-	 }
-     totalHrs+=empHrs;
-     
-     System.out.println("day :"+totalWorkingDays+" total hrs:"+totalHrs);
-     
+        this.companyName = companyName;
+        this. WAGE_PER_HR = WAGE_PER_HR;
+        this. DAY_IN_MONTH = DAY_IN_MONTH;
+        this.MAX_HRS = MAX_HRS;
     }
-    totalempwage=totalHrs*WAGE_PER_HR;
-    System.out.println(" total employee wage for "+companyName+" is "+ totalempwage);
-    
+
+    public static void calculate(){
+      int empHrs = 0;
+      int totalHrs = 0;
+      int totalWorkingDays=0;
+      
+      while (totalHrs<MAX_HRS && totalWorkingDays<DAY_IN_MONTH){
+        totalWorkingDays+=1;
+        int empcheck =(int) Math.floor((Math.random()* 10) %3 );
+        switch(empcheck){
+            case IS_FULL_TIME:
+                    empHrs=8;
+                    break;
+            case IS_HALF_TIME:
+                    empHrs=4;
+                    break;
+            default:
+	                empHrs = 0;
+	                break;
+	    }
+        totalHrs+=empHrs;
+        System.out.println("day :"+totalWorkingDays+" total hrs:"+totalHrs);
+      }
+       totalempwage=totalHrs*WAGE_PER_HR;
 	}
+    
+
+    public String toString() {
+    return (" total employee wage for "+companyName+" is "+ totalempwage);
+    }
 }
--- empwage.java
+++ empwage.java
@@ -3,6 +3,15 @@ public class empwage {
     public static void main (String[]args){
     
     ComputeWage company1= new ComputeWage("Dmart",20,20,100);
+    company1.calculate();
+    System.out.println(company1);
+
     ComputeWage company2= new ComputeWage("Relaince",20,22,200);
-  }
+    company2.calculate();
+    System.out.println(company2);
+
+    ComputeWage company3 = new ComputeWage("TCS",10,32,600);
+    company3.calculate();
+    System.out.println(company3);
+    }
 }
